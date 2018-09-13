1748088369acb823d11598bbd2a603e0abd84072
Himanshu Goyal <himanshu.goyal@live.com>
2018-09-13 19:12:31 +0530

    Setting up adapter to connect the card view with Recycler View

---
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -0,0 +1,43 @@
+package Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import Model.ListItem;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context context;
+    private List<ListItem> listItems;
+    public MyAdapter(Context context, List listitem){
+
+    }
+
+    @NonNull
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    //This will hold all the items that we have in out list row.
+    public class ViewHolder extends  RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
