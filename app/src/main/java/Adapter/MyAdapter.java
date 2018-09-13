0526a266c6c35ba4d117d8aa9c685c023296dcf9
Himanshu Goyal <himanshu.goyal@live.com>
2018-09-13 19:48:08 +0530

    Binding the view with the dataset using the adapter.

--- app/src/main/java/Adapter/MyAdapter.java
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -3,8 +3,12 @@ package Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.himanshugoyal.recyclerview.recyclerview.R;
 
 import java.util.List;
 
@@ -14,30 +18,44 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     private Context context;
     private List<ListItem> listItems;
+
     public MyAdapter(Context context, List listitem){
+        this.context = context;
+        this.listItems = listitem;
 
     }
 
     @NonNull
     @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_row, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
-
+        ListItem item = listItems.get(position); //Go and get the current position of the list
+        holder.name.setText(item.getName());
+        holder.description.setText(item.getDescription());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listItems.size();
     }
 
     //This will hold all the items that we have in out list row.
     public class ViewHolder extends  RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView description;
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.title);
+            description =(TextView) itemView.findViewById(R.id.description);
         }
     }
 }
--- app/src/main/java/com/himanshugoyal/recyclerview/recyclerview/MainActivity.java
+++ app/src/main/java/com/himanshugoyal/recyclerview/recyclerview/MainActivity.java
@@ -2,12 +2,42 @@ package com.himanshugoyal.recyclerview.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Adapter.MyAdapter;
+import Model.ListItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<ListItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewID);
+        recyclerView.setHasFixedSize(true); // so that every item has fixed size
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+
+        for (int i = 0; i< 10; i++){
+            ListItem item = new ListItem(
+                    "Item " + (i+1),
+                    "Description"
+            );
+
+            listItems.add(item);
+        }
+
+        adapter = new MyAdapter(this, listItems);
+        recyclerView.setAdapter(adapter);
     }
 }
